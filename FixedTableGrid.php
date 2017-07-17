2396bc6e72266aab8625cf1d929ce5c4c78c0228
Bhagwat Chouhan <bhagwat.chouhan@gmail.com>
2017-07-17 19:28:01 +0530

    Applied quick fixes. Added grid widgets.

---
+++ FixedTableGrid.php
@@ -0,0 +1,50 @@
+<?php
+namespace widgets\grid;
+
+/**
+ * FixedTableGrid Widget is designed to handle grid having html table with card.
+ * It can be configured to have non-scrollable fixed columns either on left or right or both sides of data table.
+ */
+class FixedTableGrid extends \cmsgears\core\common\base\TableWidget {
+
+	// Variables ---------------------------------------------------
+
+	// Globals -------------------------------
+
+	// Constants --------------
+
+	// Public -----------------
+
+	// Protected --------------
+
+	// Variables -----------------------------
+
+	// Public -----------------
+
+	public $options		= [ 'class' => 'grid-data grid-data-basic grid-table grid-table-basic grid-table-fixed' ];
+
+	public $template	= 'fixed';
+
+	// Protected --------------
+
+	// Private ----------------
+
+	// Traits ------------------------------------------------------
+
+	// Constructor and Initialisation ------------------------------
+
+	// Instance methods --------------------------------------------
+
+	// Yii interfaces ------------------------
+
+	// Yii parent classes --------------------
+
+	// yii\base\Widget --------
+
+	// CMG interfaces ------------------------
+
+	// CMG parent classes --------------------
+
+	// FixedTableGrid ------------------------
+
+}
---
+++ TableGrid.php
@@ -0,0 +1,47 @@
+<?php
+namespace widgets\grid;
+
+/**
+ * TableGrid Widget is designed to handle grid having html table with card.
+ */
+class TableGrid extends \cmsgears\core\common\base\TableWidget {
+
+	// Variables ---------------------------------------------------
+
+	// Globals -------------------------------
+
+	// Constants --------------
+
+	// Public -----------------
+
+	// Protected --------------
+
+	// Variables -----------------------------
+
+	// Public -----------------
+
+	public $options	= [ 'class' => 'grid-data grid-data-basic grid-table grid-table-basic' ];
+
+	// Protected --------------
+
+	// Private ----------------
+
+	// Traits ------------------------------------------------------
+
+	// Constructor and Initialisation ------------------------------
+
+	// Instance methods --------------------------------------------
+
+	// Yii interfaces ------------------------
+
+	// Yii parent classes --------------------
+
+	// yii\base\Widget --------
+
+	// CMG interfaces ------------------------
+
+	// CMG parent classes --------------------
+
+	// TableGrid -----------------------------
+
+}
